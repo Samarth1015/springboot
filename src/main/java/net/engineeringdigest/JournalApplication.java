d7e17c4a4d8e384e1d078af8c8acb2d923e91859
samarth <202351125@iiitvadodara.ac.in>
2025-06-07 12:15:24 +0530

    Revert "added jwt"
    This reverts commit c072c7fab7e6d5899d0063a1a541f2f9ba3fba9d.

--- src/main/java/net/engineeringdigest/JournalApplication.java
+++ src/main/java/net/engineeringdigest/JournalApplication.java
@@ -3,29 +3,16 @@ package net.engineeringdigest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableTransactionManagement
-
-
 public class JournalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
     }
-    @Bean
-    public RestTemplate restTemplate(){
-        return  new RestTemplate();
-    }
-
 
 
 }
--- src/main/java/net/engineeringdigest/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/config/SpringSecurity.java
@@ -1,26 +1,18 @@
 package net.engineeringdigest.config;
 
-import net.engineeringdigest.filter.JwtFilter;
 import net.engineeringdigest.service.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
-
-@Autowired
- private JwtFilter jwtFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,15 +21,11 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/admin/**").hasRole("admin")
                 .anyRequest().permitAll()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // No session for JWT
-
+                .httpBasic();
     http.csrf().disable();
-    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
     @Autowired
    private UserDetailsService uds;
-
-    // it is the logic to check the password match
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(uds).passwordEncoder(Bcrypt());
@@ -46,12 +34,5 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     @Bean
     public static PasswordEncoder Bcrypt(){
         return new BCryptPasswordEncoder();
-    }
-
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 }
